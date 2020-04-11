0371b107240e9d5318966457b545bbfb065f8e50
Kyle Jones <rmbart395@yahoo.com>
2020-04-11 14:50:24 -0600

    Created background fade to complete iteration 1

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ function adaptLayout() {
 
 function orginalLayout() {
   menu.childNodes[7].classList.remove('hidden')
-  menu.childNodes[1].classList.add('hidden')  
+  menu.childNodes[1].classList.add('hidden')
 }
 
 function adaptLayout() {
@@ -33,26 +33,28 @@ function showMenu(event) {
     menu.childNodes[1].classList.add('hidden');
     menu.childNodes[3].classList.remove('hidden');
     menu.childNodes[7].classList.remove('hidden');
+    var ideasSection = document.querySelector(".ideas");
+    ideasSection.classList.add('faded');
   }
+
   showMenuContent();
 }
 
 function showMenuContent() {
-  menu.style.cssText = "grid-template-rows: 8vh 20vh" 
+  menu.style.cssText = "grid-template-rows: 8vh 20vh"
 }
 
 function hideMenu (event) {
   if (event.target === menu.childNodes[3]) {
     menu.childNodes[3].classList.add('hidden');
     menu.childNodes[1].classList.remove('hidden');
+    menu.childNodes[7].classList.add('hidden');
+    var ideasSection = document.querySelector(".ideas");
+    ideasSection.classList.remove('faded');
   }
+  hideMenuContent();
 }
 
-// add function to the hamburger button
-// display hidden filter star Ideas
-// 1. add addEventListener to querySelector for 'menu'
-// 2. click event on hamburger btn should display the filter star idea section
-// 3. it should also display x icon and hide hamburger button
-// 4. the main body contents should fade/contrast or become opaque (pg.254 in css book)
-// 5. when we click on the x the filter star idea section should hide
-// 6. after the idea section is hidden the hamburger btn should come back to the display
+function hideMenuContent () {
+  menu.style.cssText = "grid-template-rows: auto"
+}
