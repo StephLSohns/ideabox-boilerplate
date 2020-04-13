5b59714f19c6ce8d67a32a4019864f5a987521d9
emontealvo <edwin.montealvo@gmail.com>
2020-04-12 22:32:41 -0600

    Refactor a couple of functions and css style rules

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ var titleInput = document.getElementById('title-input-field');
 var bodyInput = document.getElementById('body-input-field');
 var saveBtn = document.getElementById('save-btn-id');
 var ideaCardsSection = document.querySelector('.idea-cards-section')
+var ideasSection = document.querySelector(".ideas");
+// var ideaForm = ideaFormSection.querySelector(".idea-form")
 
 var userIdeas = [];
 var idea;
@@ -20,11 +22,13 @@ menu.addEventListener('click', function() {
 
 ideaFormSection.addEventListener('click', ideaFormButtonHandler)
 ideaFormSection.addEventListener('keyup', enableSaveBtn);
-
+ideaCardsSection.addEventListener('click', function() {
+  // starIdea(),
+  deleteIdea(event)
+  // commentIdea()
+})
 
 window.onload = adaptLayout();
-
-
 
 function adaptLayout() {
   if (!mqMedium.matches) {
@@ -56,7 +60,6 @@ function showMenu(event) {
     menu.childNodes[1].classList.add('hidden');
     menu.childNodes[3].classList.remove('hidden');
     menu.childNodes[7].classList.remove('hidden');
-    var ideasSection = document.querySelector(".ideas");
     ideasSection.classList.add('faded');
   }
   showMenuContent();
@@ -71,85 +74,44 @@ function hideMenu (event) {
     menu.childNodes[3].classList.add('hidden');
     menu.childNodes[1].classList.remove('hidden');
     menu.childNodes[7].classList.add('hidden');
-    var ideasSection = document.querySelector(".ideas");
     ideasSection.classList.remove('faded');
   }
   hideMenuContent();
 }
-// by accessing the body and title input variables have value I want to tell
-// the button to enable when both of those input fields have text in them
-// if either one of them is empty the save button remains disable
+
+function hideMenuContent () {
+  menu.style.cssText = "grid-template-rows: auto"
+}
+
 function enableSaveBtn() {
   if (titleInput.value !== '' && bodyInput.value !== '') {
     saveBtn.disabled = false;
   }
 }
 
-
-function hideMenuContent () {
-  menu.style.cssText = "grid-template-rows: auto"
-}
-
-
-// add function to the hamburger button
-// display hidden filter star Ideas
-// 1. add addEventListener to querySelector for 'menu'
-// 2. click event on hamburger btn should display the filter star idea section
-// 3. it should also display x icon and hide hamburger button
-// 4. the main body contents should fade/contrast or become opaque (pg.254 in css book)
-// 5. when we click on the x the filter star idea section should hide
-// 6. after the idea section is hidden the hamburger btn should come back to the display
-
-
-// Iteration 2 - Adding Ideas
-
-
-// As a user,
-// - When I click “Save”,
-
-// Save is a button inside the idea-form form element inside the idea-form-section section element
-//    it has a class name of save-idea-btn
-//    what is the most efficient way to access this button ??????
-
 function ideaFormButtonHandler(event) {
   if (event.target.className === "save-idea-btn") {
-      validateInputFields()
-      submitIdeaForm();
-      // we can add any other functions related to our event here.
+    createIdea();
+    submitIdeaForm();
   }
-  // event.preventDefault();
 }
+
 function submitIdeaForm() {
-  var ideaForm = ideaFormSection.querySelector(".idea-form")
-  if (ideaForm.submit) {
-    ideaForm.reset();
-  saveBtn.disabled = true;
+  if (document.forms[0].submit) {
+    document.forms[0].reset();
+    saveBtn.disabled = true;
+    event.preventDefault();
   }
 }
-// - If I entered information in both the “Title” and “Body” input fields,
 
-    // This means some form of input validation, still kind of iffy on this, but the number guess game
-    //    we were asked to refactor may provide some insigth within the mess of functions. The event chapter
-    //    in JS book is basically built around a validation function and events, also good resource.
-
-
-
-function validateInputFields() {
-  var formInputs = ideaFormSection.getElementsByTagName("input");
-  if (formInputs[0].value === '' || formInputs[1].value === '') {
-    alert('Please, share your Ideas :) save button enabler function may go here, not sure yet.' )
-  } else {
-    idea = new Idea (`${formInputs[0].value}`, `${formInputs[1].value}`);
-    userIdeas.push(idea);
-    renderIdea(idea);
-  }
+function createIdea() {
+  var title = document.forms[0].elements[0].value
+  var body = document.forms[0].elements[1].value
+  idea = new Idea (`${title}`, `${body}`);
+  userIdeas.push(idea);
+  renderIdea(idea);
   event.preventDefault();
-}
-
-// - I should see a new idea card with the provided title and body appear in the idea list
-
-//  after inputs are validated, it should become a new instance of our Idea object defined in Idea.js
-//  as a new Idea instance, it should become displayed in ou idea-cards section.
+  }
 
 function renderIdea (idea) {
   var ideaBox = document.createElement('section');
@@ -179,7 +141,7 @@ function createStarIcon(topBarParentDiv) {
 function createDeleteIcon(topBar) {
   var deleteIcon = document.createElement('button');
   deleteIcon.setAttribute('id', 'delete-icon');
-  deleteIcon.disabled = true;
+  deleteIcon.disabled = false;
   topBar.appendChild(deleteIcon)
 }
 
@@ -210,67 +172,13 @@ function createCommentButton(card) {
   card.appendChild(commentButton);
 }
 
+// function deleteIdea(event) {
+//   console.log(event.path[2].id)
+  
+// }
 
 
 
 
 
-// As a user,
-// - When I click “Save”,
 
-    // Same as above
-
-// - If I entered information in both the “Title” and “Body” input fields,
-
-    //  Since we are also validating data here, it is an indication that form validation is handled by one function
-    //  This function, when validation is true will run a number of different functions namely the one above and the following one:
-
-// - I should see the “Title” and “Body” input fields clear out
-
-    // Once the idea card has been created (simply means invoked after the function above), both input field under the
-    //  idea-form form element should be reset to be empty.
-
-
-
-
-// As a user,
-// - When I look at the “Save” button,
-
-    //  The Save button will handle many events it seems, this just means running seperate functions around the same event
-    //      it will be important to rememeber "Single responsibility principles"  and how you can use scope and event delegation to your advantage
-    //      What's more efficient having query selectors for different elements and each have their own event listeners or have a parent element with multiple event listeners?
-    //      From the lessons it would seem that the latter is preferred.
-    //      This button is inside the form element which also holds the inputs to be validated. The form (parent) element seems like a good candidates, but looking ahead
-    //      eventually we will have to make use of the search bar as well (in it's seperate div parent element) which is contained by the Dave button's
-    //      grandparent idea-form-section
-
-
-// - When either the “Title” or “Body” inputs are empty,
-
-    //  Again, the guess number game might be of good reference here, albeit not perfect (not just patter matching)
-    //  Similar to the conditions above, if those conditions are note me (i.e. else if (oppositeCondition)) do the following;
-
-
-// - I should notice that the “Save” button is disabled because it is a lighter color and the cursor is not a pointer when I hover over it
-
-      // the "Save" button should be disabled. The styling (in CSS using pseudo-class :disabled) should be different. We will use the previous
-      //  validations and JS to toggle the disabled feature when relevant. (relevantElement.disabled = true or false is the kind of documentation that I have seen. Will requiere further probing)
-
-
-
-
-// As a user,
-// - When I click “Save”,
-      // When broken down like this, javascipt seems like an uber-condensed way to storytell.
-      // A lot is center around this "Save" button.
-
-// - And a new card is successfully created,
-      // I believe successfully created may mean creating a new Idea object instance and have that instance stored in local storage
-      //  (the local storage bit may come at a later iteration)
-      //    For now, it means the object instance has been created, and like our hang in there posters, show up within our session within the idea-cards-section,
-      //    which could mean being stored in some array within a global variable.
-
-
-// - I should NOT see the page reload
-
-    //  event.preventDefault() but what event, where to place it, and how to apply it are the big questions.
